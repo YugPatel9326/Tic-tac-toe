3c156aafcd8809be84c78550fce1e095146e0c94
YugPatel9326 <py41822@mail.harpercollege.edu>
2025-10-27 00:03:47 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,99 +1,41 @@
 #include <iostream>
-#include <vector>
 #include <string>
 using namespace std;
 
-void printBoard(const vector<char>& b) {
-    cout << " " << b[0] << " | " << b[1] << " | " << b[2] << "\n";
-    cout << "-----------\n";
-    cout << " " << b[3] << " | " << b[4] << " | " << b[5] << "\n";
-    cout << "-----------\n";
-    cout << " " << b[6] << " | " << b[7] << " | " << b[8] << "\n";
-}
-
-bool isValidMove(const vector<char>& b, int move) {
-    return move >= 1 && move <= 9 && b[move - 1] != 'X' && b[move - 1] != 'O';
-}
-
-int getPlayerMove(const vector<char>& board, char player) {
-    while (true) {
-        cout << "Player " << player << ", enter your move (1-9): ";
-        string input;
-        cin >> input;
-        cin.ignore(1000, '\n');
-
-        bool onlyDigits = true;
-        if (input.size() > 3) {             
-            onlyDigits = false;
-        } else {
-            for (char ch : input) {
-                if (ch < '0' || ch > '9') {
-                    onlyDigits = false;
-                    break;
-                }
-            }
-        }
-
-        if (!onlyDigits) {
-            cout << "You must enter a number between 1 and 9\n";
-            continue;
-        }
-
-        int move = stoi(input);              
-        if (!isValidMove(board, move)) {
-            cout << "Your move is not valid. Please try again\n";
-            continue;
-        }
-        return move;
-    }
-}
-
-int checkWin(const vector<char>& b) {
-    const int W[8][3] = {
-        {0,1,2}, {3,4,5}, {6,7,8}, 
-        {0,3,6}, {1,4,7}, {2,5,8}, 
-        {0,4,8}, {2,4,6}           
-    };
-    for (int i = 0; i < 8; ++i) {
-        int a = W[i][0], c = W[i][1], d = W[i][2];
-        if (b[a] == b[c] && b[c] == b[d]) {
-            return (b[a] == 'X') ? 1 : 2;
-        }
-    }
-    return 0;
-}
-
-bool boardFull(const vector<char>& b) {
-    for (char cell : b) {
-        if (cell != 'X' && cell != 'O') return false;
-    }
-    return true;
-}
+void playRegularGame();
+void playBattleGame();
 
 int main() {
-    vector<char> board(9);
-    for (int i = 0; i < 9; ++i) board[i] = '1' + i;
-
-    char current = 'X';
     while (true) {
-        printBoard(board);
-
-        int move = getPlayerMove(board, current);
-        board[move - 1] = current;
-
-        int winner = checkWin(board);
-        if (winner != 0) {
-            printBoard(board);
-            cout << "Player " << (winner == 1 ? 'X' : 'O') << " is the winner!\n";
-            break;
+        cout << "Welcome to Tic-Tac-Toe\n";
+        cout << "1) Regular Tic-Tac-Toe\n";
+        cout << "2) Battle Tic-Tac-Toe\n";
+        cout << "Choose 1 or 2: ";
+        string choice;
+        cin >> choice;
+        cin.ignore(1000, '\n');
+        if (choice != "1" && choice != "2") {
+            cout << "Invalid choice. Try again.\n";
+            continue;
         }
-        if (boardFull(board)) {
-            printBoard(board);
-            cout << "It's a draw!\n";
-            break;
+        if (choice == "1") {
+            playRegularGame();
+        } else {
+            playBattleGame();
         }
-        current = (current == 'X') ? 'O' : 'X';
+
+        while (true) {
+            cout << "Play again? (y/n): ";
+            string again;
+            cin >> again;
+            cin.ignore(1000, '\n');
+            if (again == "y" || again == "Y") break; 
+            if (again == "n" || again == "N") {
+                cout << "Goodbye.\n";
+                return 0;
+            }
+            cout << "Please enter y or n.\n";
+        }
     }
-
     return 0;
 }
